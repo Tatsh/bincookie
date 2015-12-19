552e9e291bfc91add205f72e935365b513d98a62
Andrew Udvare <audvare@gmail.com>
2015-12-19 13:34:23 -0800

    Output information in Netscape format

--- test/main.c
+++ test/main.c
@@ -11,10 +11,22 @@ int main(int argc, char *argv[]) {
 
     binarycookies_t *bc = binarycookies_init(argv[1]);
     unsigned int i, j;
+    uint32_t flags;
 
+    // Output in Netscape cookies.txt format
     for (i = 0; i < bc->num_pages; i++) {
         for (j = 0; j < bc->pages[i]->number_of_cookies; j++) {
-            fprintf(stderr, "%s=%s; %s; %s; %s; expires %s\n", bc->pages[i]->cookies[j]->name, bc->pages[i]->cookies[j]->value, bc->pages[i]->cookies[j]->url, bc->pages[i]->cookies[j]->path, bc->pages[i]->cookies[j]->flags_str, bc->pages[i]->cookies[j]->expiration_date_str);
+            flags = bc->pages[i]->cookies[j]->flags;
+
+            // domain, flag, path, secure, expiration, name, value
+            printf("%s\t%s\t%s\t%s\t%.f\t%s\t%s\n",
+                   bc->pages[i]->cookies[j]->url,
+                   bc->pages[i]->cookies[j]->url[0] == '.' ? "TRUE" : "FALSE",
+                   bc->pages[i]->cookies[j]->path,
+                   flags == 1 || flags == 5 ? "TRUE" : "FALSE",
+                   bc->pages[i]->cookies[j]->expiration_date,
+                   bc->pages[i]->cookies[j]->name,
+                   bc->pages[i]->cookies[j]->value);
         }
     }
 
