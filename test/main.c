c9624beb971cc2d5beaa1b2ead6b03daf0fa822f
Andrew Udvare <audvare@gmail.com>
2015-12-19 13:12:48 -0800

    Print out information about cookies

--- test/main.c
+++ test/main.c
@@ -10,6 +10,14 @@ int main(int argc, char *argv[]) {
     }
 
     binarycookies_t *bc = binarycookies_init(argv[1]);
+    unsigned int i, j;
+
+    for (i = 0; i < bc->num_pages; i++) {
+        for (j = 0; j < bc->pages[i]->number_of_cookies; j++) {
+            fprintf(stderr, "%s=%s; %s; %s; %s; expires %s\n", bc->pages[i]->cookies[j]->name, bc->pages[i]->cookies[j]->value, bc->pages[i]->cookies[j]->url, bc->pages[i]->cookies[j]->path, bc->pages[i]->cookies[j]->flags_str, bc->pages[i]->cookies[j]->expiration_date_str);
+        }
+    }
+
     binarycookies_free(bc);
 
     return 0;
