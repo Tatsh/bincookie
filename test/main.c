15dcbcce4e643eaa3970e4fe296624d5fca68e16
Andrew Udvare <audvare@gmail.com>
2015-12-19 20:54:38 -0800

    Update example

--- test/main.c
+++ test/main.c
@@ -16,12 +16,12 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < bc->num_pages; i++) {
         for (j = 0; j < bc->pages[i]->number_of_cookies; j++) {
             // domain, flag, path, secure, expiration, name, value
-            printf("%s\t%s\t%s\t%s\t%.f\t%s\t%s\n",
+            printf("%s\t%s\t%s\t%s\t%ld\t%s\t%s\n",
                    bc->pages[i]->cookies[j]->domain,
                    binarycookies_domain_access_full(bc->pages[i]->cookies[j]) ? "TRUE" : "FALSE",
                    bc->pages[i]->cookies[j]->path,
                    binarycookies_is_secure(bc->pages[i]->cookies[j]) ? "TRUE" : "FALSE",
-                   bc->pages[i]->cookies[j]->expiration_date,
+                   bc->pages[i]->cookies[j]->expiration_time,
                    bc->pages[i]->cookies[j]->name,
                    bc->pages[i]->cookies[j]->value);
         }
