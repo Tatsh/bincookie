157134c450bcfc6b4c53f6a7bbb86ed2baecdffe
Andrew Udvare <audvare@gmail.com>
2017-01-12 01:34:54 -0500

    Fix documentation

--- bincookie.h
+++ bincookie.h
@@ -1,3 +1,8 @@
+/**
+ * @file bincookie.h
+ * @brief A parser for Apple's proprietary binary cookie storage format.
+ */
+
 #ifndef _BINCOOKIE_H
 #define _BINCOOKIE_H
 
@@ -33,20 +38,20 @@ typedef unsigned __int32 uint32_t;
 
 //! Check if a cookie has the secure bit enabled (only to be accessed over HTTPS).
 /*!
- * \param cookie_ptr Pointer to a cookie data structure.
+ * \param cookie_ptr Pointer to a binarycookies_cookie_t data structure.
  */
-#define binarycookies_is_secure(cookie_ptr) (cookie_ptr->flags & secure)
+#define binarycookies_is_secure(cookie_ptr) (cookie_ptr->flags & secure) /**< @hideinitializer */
 //! Check if a cookie can be used on all subdomains of its original domain.
 /*!
- * \param cookie_ptr Pointer to a cookie data structure.
+ * \param cookie_ptr Pointer to a binarycookies_cookie_t structure.
  */
-#define binarycookies_domain_access_full(cookie_ptr) (cookie_ptr->domain && cookie_ptr->domain[0] == '.')
+#define binarycookies_domain_access_full(cookie_ptr) (cookie_ptr->domain && cookie_ptr->domain[0] == '.') /**< @hideinitializer */
 
 //! Security enabled for a cookie.
 /*! A cookie can be either or these values or 2 or more values OR'd together. */
 typedef enum {
-    secure = 1,
-    http_only = 1 << 2,
+    secure = 1,         /**< Cookie can only be used with HTTPS @hideinitializer */
+    http_only = 1 << 2, /**< Cookie cannot be accessed by JavaScript @hideinitializer */
 } binarycookies_flag;
 
 //! Cookie data structure.
@@ -66,7 +71,7 @@ typedef struct {
 
 //! Cookie page structure. A page can consist of 1 or more cookies.
 typedef struct {
-    unsigned char unk1[4];             /*! Has unknown purpose. Seems to be always `0x0 0x0 0x1 0x0`. */
+    unsigned char unk1[4];             /*!< Has unknown purpose. Seems to be always `0x0 0x0 0x1 0x0`. */
     uint32_t number_of_cookies;        /*!< Number of cookies contained in this page. */
     binarycookies_cookie_t **cookies; /*!< Pointer to an array of cookies. */
 } binarycookies_page_t;
@@ -83,12 +88,12 @@ typedef struct {
 //! Read a binarycookies file.
 /*!
   \param file_path File path string.
- \return Pointer to a <code>binarycookies_t</code> structure or <code>NULL</code>.
+ \return Pointer to a binarycookies_t structure or `NULL`.
  */
 ADDAPI binarycookies_t *binarycookies_init(const char *file_path);
 //! Free a binarycookies_t structure.
 /*!
-  \param cfile Pointer to data structure created with <code>binarycookies_init()</code>.
+  \param cfile Pointer to data structure created with binarycookies_init().
  */
 ADDAPI void binarycookies_free(binarycookies_t *cfile);
 
