79ef57f6c06fe050f84977594f5f1828856d5634
Andrew Udvare <audvare@gmail.com>
2019-03-05 19:32:02 -0500

    Use one contiguous block of memory; closes #1

--- bincookie.c
+++ bincookie.c
@@ -1,13 +1,12 @@
 // Based on the Python script by Satishb3 <satishb3@securitylearn.net>
 
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "bincookie.h"
-
-static const unsigned long APPLE_EPOCH_OFFSET = 978307200;
 
 // https://msdn.microsoft.com/en-us/library/a3140177.aspx
 #if defined(_MSC_VER)
@@ -18,17 +17,13 @@ static const unsigned long APPLE_EPOCH_OFFSET = 978307200;
 #endif // _MSC_VER
 
 EXPORT
-bincookie_t *bincookie_init(const char *file_path) {
-    unsigned int i, j;
-    signed int slen;
-    uint32_t *cookie_offsets;
+bincookie_t *const bincookie_init(const char *file_path) {
     char magic[4];
     size_t read;
 
     FILE *binary_file = fopen(file_path, "rb");
 
     if (!binary_file) {
-        fprintf(stderr, "Unable to open file: %s\n", file_path);
         return NULL;
     }
 
@@ -36,173 +31,57 @@ bincookie_t *bincookie_init(const char *file_path) {
     assert(read == 1);
 
     if (strncmp(magic, "cook", sizeof(magic))) {
-        fprintf(stderr, "Not a correctly formatted file (incorrect magic)\n");
+        errno = EIO;
         fclose(binary_file);
         return NULL;
     }
 
-    bincookie_t *cfile = malloc(sizeof(bincookie_t));
-    if (!cfile) {
-        fprintf(stderr, "Failed to allocate memory.\n");
-        fclose(binary_file);
-        return NULL;
-    }
-    memset(cfile, 0, sizeof(bincookie_t));
-    memcpy(cfile->magic, magic, 4);
-
-    read = fread(&cfile->num_pages, sizeof(uint32_t), 1, binary_file); // big endian
-    assert(read == 1);
-    cfile->num_pages = __builtin_bswap32(cfile->num_pages);
-    cfile->raw_pages = malloc(sizeof(char *) * cfile->num_pages);
-    memset(cfile->raw_pages, 0, sizeof(char *) * cfile->num_pages);
-    cfile->pages = malloc(sizeof(bincookie_page_t *) * cfile->num_pages);
-    memset(cfile->pages, 0, sizeof(bincookie_page_t *) * cfile->num_pages);
-
-    // size: num_pages * sizeof(int), each page size is big endian
-    cfile->page_sizes = malloc(sizeof(uint32_t) * cfile->num_pages);
-    memset(cfile->page_sizes, 0, sizeof(uint32_t) * cfile->num_pages);
-    for (i = 0; i < cfile->num_pages; i++) {
-        uint32_t page_size;
-
-        read = fread(&page_size, sizeof(uint32_t), 1, binary_file);
-        assert(read == 1);
-        cfile->page_sizes[i] = __builtin_bswap32(page_size);
-    }
-
-    for (i = 0; i < cfile->num_pages; i++) {
-        char *ps = malloc(cfile->page_sizes[i]);
-        memset(ps, 0, cfile->page_sizes[i]);
-
-        read = fread(ps, cfile->page_sizes[i], 1, binary_file);
-        assert(read == 1);
-        cfile->raw_pages[i] = ps;
-    }
-
+    // Read entire file
+    fseek(binary_file, 0, SEEK_END);
+    size_t num_bytes = (size_t)ftell(binary_file);
+    bincookie_t *cook = malloc(num_bytes);
+    memset(cook, 0, num_bytes);
+    assert(cook != NULL);
+    rewind(binary_file);
+    fread(cook, 1, num_bytes, binary_file);
+    cook->num_pages = __builtin_bswap32(cook->num_pages);
     fclose(binary_file);
 
-    // Read
-    for (i = 0; i < cfile->num_pages; i++) {
-        bincookie_page_t *page = malloc(sizeof(bincookie_page_t));
-        memset(page, 0, sizeof(bincookie_page_t));
-        memcpy(page->unk1, cfile->raw_pages[i], 4);
-
-        char *page_ptr = cfile->raw_pages[i] + 4; // skip 4 bytes, always 0x0 0x0 0x1 0x0
-
-        memcpy(&page->number_of_cookies, page_ptr, sizeof(uint32_t));
-        page_ptr += sizeof(uint32_t);
-
-        cookie_offsets = malloc(sizeof(uint32_t) * page->number_of_cookies);
-        memset(cookie_offsets, 0, sizeof(uint32_t) * page->number_of_cookies);
-        page->cookies = malloc(sizeof(bincookie_cookie_t) * page->number_of_cookies);
-        memset(page->cookies, 0, sizeof(bincookie_cookie_t) * page->number_of_cookies);
-        for (j = 0; j < page->number_of_cookies; j++) {
-            uint32_t cookie_offset;
-
-            memcpy(&cookie_offset, page_ptr, sizeof(uint32_t));
-            cookie_offsets[j] = cookie_offset;
-            page_ptr += sizeof(uint32_t);
-        }
-
-        for (j = 0; j < page->number_of_cookies; j++) {
-            page_ptr = cfile->raw_pages[i] + cookie_offsets[j];
-            bincookie_cookie_t *cookie = malloc(sizeof(bincookie_cookie_t));
-            memset(cookie, 0, sizeof(bincookie_cookie_t));
-            page->cookies[j] = cookie;
-
-            memcpy(cookie, page_ptr, sizeof(uint32_t) + 4 + sizeof(uint32_t) + 4);
-            page_ptr += sizeof(uint32_t) + 4 + sizeof(uint32_t) + 4;
-
-            uint32_t url_offset;
-            memcpy(&url_offset, page_ptr, sizeof(uint32_t));
-            page_ptr += sizeof(uint32_t);
-
-            uint32_t name_offset;
-            memcpy(&name_offset, page_ptr, sizeof(uint32_t));
-            page_ptr += sizeof(uint32_t);
-
-            uint32_t path_offset;
-            memcpy(&path_offset, page_ptr, sizeof(uint32_t));
-            page_ptr += sizeof(uint32_t);
-
-            uint32_t value_offset;
-            memcpy(&value_offset, page_ptr, sizeof(uint32_t));
-            page_ptr += sizeof(uint32_t);
-
-            page_ptr += sizeof(uint32_t) * 2; // end of cookie
-
-            double ce_time;
-            memcpy(&ce_time, page_ptr, sizeof(double));
-            cookie->expiration_time = (time_t)ce_time + APPLE_EPOCH_OFFSET;
-            page_ptr += sizeof(double);
-
-            memcpy(&ce_time, page_ptr, sizeof(double));
-            cookie->creation_time = (time_t)ce_time + APPLE_EPOCH_OFFSET;
-
-            page_ptr = cfile->raw_pages[i] + cookie_offsets[j] + url_offset;
-            slen = name_offset - url_offset;
-            if (slen > 0) {
-                cookie->domain = malloc(slen);
-                memset(cookie->domain, 0, slen);
-                memcpy(cookie->domain, page_ptr, slen);
-            }
-
-            page_ptr = cfile->raw_pages[i] + cookie_offsets[j] + name_offset;
-            slen = path_offset - name_offset;
-            if (slen > 0) {
-                cookie->name = malloc(slen);
-                memset(cookie->name, 0, slen);
-                memcpy(cookie->name, page_ptr, slen);
-            }
-
-            page_ptr = cfile->raw_pages[i] + cookie_offsets[j] + path_offset;
-            slen = value_offset - path_offset;
-            if (slen > 0) {
-                cookie->path = malloc(slen);
-                memset(cookie->path, 0, slen);
-                memcpy(cookie->path, page_ptr, slen);
-            }
-
-            page_ptr = cfile->raw_pages[i] + cookie_offsets[j] + value_offset;
-            slen = 0;
-            int k = 0;
-            while (page_ptr[k] != 0) {
-                slen += 1;
-                k++;
-            }
-            if (slen > 0) {
-                cookie->value = malloc(slen);
-                memset(cookie->value, 0, slen);
-                memcpy(cookie->value, page_ptr, slen);
-            }
-        }
-
-        free(cookie_offsets);
-
-        cfile->pages[i] = page;
+    // Fix page size numbers
+    for (uint32_t i = 0; i < cook->num_pages; i++) {
+        *(cook->page_sizes + i) = __builtin_bswap32(*(cook->page_sizes + i));
     }
 
-    return cfile;
+    return cook;
 }
 
 EXPORT
-void bincookie_free(bincookie_t *cfile) {
-    unsigned int i, j;
-
-    for (i = 0; i < cfile->num_pages; i++) {
-        free(cfile->raw_pages[i]);
-
-        for (j = 0; j < cfile->pages[i]->number_of_cookies; j++) {
-            free(cfile->pages[i]->cookies[j]->domain);
-            free(cfile->pages[i]->cookies[j]->name);
-            free(cfile->pages[i]->cookies[j]->path);
-            free(cfile->pages[i]->cookies[j]->value);
-            free(cfile->pages[i]->cookies[j]);
-        }
-        free(cfile->pages[i]->cookies);
-        free(cfile->pages[i]);
+bincookie_page_t *const
+bincookie_iter_pages(const bincookie_t *bc,
+                     bincookie_iter_state_t *const state) {
+    if (state->page_offset == 0) {
+        state->page_offset = (uint32_t)(4 + sizeof(uint32_t) +
+                                        (sizeof(uint32_t) * bc->num_pages));
     }
-    free(cfile->pages);
-    free(cfile->raw_pages);
-    free(cfile->page_sizes);
-    free(cfile);
+    if (state->page_index < bc->num_pages) {
+        bincookie_page_t *page =
+            (bincookie_page_t *)((char *)bc + state->page_offset);
+        state->page_offset += bc->page_sizes[state->page_index];
+        state->page_index++;
+        return page;
+    }
+    return NULL;
+}
+
+EXPORT
+bincookie_cookie_t *const bincookie_iter_cookies(const bincookie_page_t *page,
+                                                 unsigned int *cookie_index) {
+    if (*cookie_index < page->num_cookies) {
+        bincookie_cookie_t *ptr =
+            (bincookie_cookie_t *)((char *)page +
+                                   page->cookie_offsets[*cookie_index]);
+        (*cookie_index)++;
+        return ptr;
+    }
+    return NULL;
 }
--- bincookie.h
+++ bincookie.h
@@ -12,8 +12,8 @@ extern "C" {
 
 // https://github.com/nodejs/http-parser/blob/master/http_parser.h#L32
 #include <sys/types.h>
-#if defined(_WIN32) && !defined(__MINGW32__) && \
-  (!defined(_MSC_VER) || _MSC_VER<1600) && !defined(__WINE__)
+#if defined(_WIN32) && !defined(__MINGW32__) &&                               \
+    (!defined(_MSC_VER) || _MSC_VER < 1600) && !defined(__WINE__)
 #include <BaseTsd.h>
 #include <stddef.h>
 typedef unsigned __int32 uint32_t;
@@ -28,31 +28,55 @@ typedef unsigned __int32 uint32_t;
 #define ADDAPI
 #endif
 
-// Disable warnings about secure CRT, but still enable compatible signature versions
-// http://stackoverflow.com/a/119752/374110
+// Disable warnings about secure CRT, but still enable compatible signature
+// versions http://stackoverflow.com/a/119752/374110
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
 #include <time.h>
 
-//! Check if a cookie has the secure bit enabled (only to be accessed over HTTPS).
+//! Check if a cookie has the secure bit enabled (only to be accessed over
+//! HTTPS).
 /*!
  * \param cookie_ptr Pointer to a bincookie_cookie_t data structure.
  */
-#define bincookie_is_secure(cookie_ptr) (cookie_ptr->flags & secure) /**< @hideinitializer */
+#define bincookie_is_secure(cookie_ptr)                                       \
+    (cookie_ptr->flags & secure) /**< @hideinitializer */
 //! Check if a cookie can be used on all subdomains of its original domain.
 /*!
  * \param cookie_ptr Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_domain_access_full(cookie_ptr) (cookie_ptr->domain && cookie_ptr->domain[0] == '.') /**< @hideinitializer */
+#define bincookie_domain_access_full(cookie_ptr)                              \
+    (((char *)cookie_ptr + cookie_ptr->domain_offset)[0] ==                   \
+     '.') /**< @hideinitializer */
+
+#define bincookie_iter_state_init(s)                                          \
+    s.page_offset = 0;                                                        \
+    s.page_index = 0;
+#define bincookie_domain(c) ((char *)c + c->domain_offset)
+#define bincookie_path(c) ((char *)c + c->path_offset)
+#define bincookie_name(c) ((char *)c + c->name_offset)
+#define bincookie_value(c) ((char *)c + c->value_offset)
+#define APPLE_EPOCH_OFFSET 978307200
+#define bincookie_expiration_time(c)                                          \
+    (c->expiry_date_epoch + APPLE_EPOCH_OFFSET)
+#define bincookie_creation_time(c) (c->create_date_epoch + APPLE_EPOCH_OFFSET)
 
 //! Security enabled for a cookie.
-/*! A cookie can be either or these values or 2 or more values OR'd together. */
+/*! A cookie can be either or these values or 2 or more values OR'd together.
+ */
 typedef enum {
-    secure = 1,         /**< Cookie can only be used with HTTPS @hideinitializer */
-    http_only = 1 << 2, /**< Cookie cannot be accessed by JavaScript @hideinitializer */
+    secure = 1, /**< Cookie can only be used with HTTPS @hideinitializer */
+    http_only =
+        1
+        << 2, /**< Cookie cannot be accessed by JavaScript @hideinitializer */
 } bincookie_flag;
+
+typedef struct {
+    uint32_t page_offset;
+    uint32_t page_index;
+} bincookie_iter_state_t;
 
 //! Cookie data structure.
 typedef struct {
@@ -61,28 +85,30 @@ typedef struct {
     bincookie_flag flags; /*!<Flags used with the cookie. */
     unsigned char unk2[4];
 
-    time_t creation_time;   /*!< Creation time. */
-    time_t expiration_time; /*!< Expiration time. */
-    char *domain;           /*!< Domain name. Can be NULL. */
-    char *name;             /*!< Name of the cookie. */
-    char *path;             /*!< Path of the cookie. Can be NULL. */
-    char *value;            /*!< Value of the cookie. */
+    uint32_t domain_offset;
+    uint32_t name_offset;
+    uint32_t path_offset;
+    uint32_t value_offset;
+
+    unsigned char unk[8];
+
+    double expiry_date_epoch;
+    double create_date_epoch;
 } bincookie_cookie_t;
 
 //! Cookie page structure. A page can consist of 1 or more cookies.
 typedef struct {
-    unsigned char unk1[4];             /*!< Has unknown purpose. Seems to be always `0x0 0x0 0x1 0x0`. */
-    uint32_t number_of_cookies;        /*!< Number of cookies contained in this page. */
-    bincookie_cookie_t **cookies; /*!< Pointer to an array of cookies. */
+    unsigned char unk1[4]; /*!< Has unknown purpose. Seems to be always `0x0
+                              0x0 0x1 0x0`. */
+    uint32_t num_cookies;  /*!< Number of cookies contained in this page. */
+    uint32_t cookie_offsets[];
 } bincookie_page_t;
 
 //! bincookie file structure.
 typedef struct {
     unsigned char magic[4]; /*!< Magic: "cook" */
-    uint32_t num_pages; /*!< Number of pages in this file */
-    uint32_t *page_sizes; /*!< Page sizes (same length as number of pages) */
-    char **raw_pages;
-    bincookie_page_t **pages; /*!< Array of cookie pages. */
+    uint32_t num_pages;     /*!< Number of pages in this file */
+    uint32_t page_sizes[];  /*!< Page sizes (same length as number of pages) */
 } bincookie_t;
 
 //! Read a bincookie file.
@@ -90,12 +116,11 @@ typedef struct {
   \param file_path File path string.
  \return Pointer to a bincookie_t structure or `NULL`.
  */
-ADDAPI bincookie_t *bincookie_init(const char *file_path);
-//! Free a bincookie_t structure.
-/*!
-  \param cfile Pointer to data structure created with bincookie_init().
- */
-ADDAPI void bincookie_free(bincookie_t *cfile);
+ADDAPI bincookie_t *const bincookie_init(const char *);
+ADDAPI bincookie_page_t *const
+bincookie_iter_pages(const bincookie_t *, bincookie_iter_state_t *const);
+ADDAPI bincookie_cookie_t *const
+bincookie_iter_cookies(const bincookie_page_t *, unsigned int *);
 
 #ifdef __cplusplus
 }
