8e0a772a8f78fb29764740b3c98150304ef37655
Andrew Udvare <audvare@gmail.com>
2023-09-19 15:18:01 -0400

    bincookie: fix argument order in fread; lint

--- bincookie.h
+++ bincookie.h
@@ -14,8 +14,8 @@ extern "C" {
 
 // https://github.com/nodejs/http-parser/blob/master/http_parser.h#L32
 #include <sys/types.h>
-#if defined(_WIN32) && !defined(__MINGW32__) &&                               \
-    (!defined(_MSC_VER) || _MSC_VER < 1600) && !defined(__WINE__)
+#if defined(_WIN32) && !defined(__MINGW32__) && (!defined(_MSC_VER) || _MSC_VER < 1600) &&         \
+    !defined(__WINE__)
 #include <BaseTsd.h>
 #include <stddef.h>
 typedef unsigned __int32 uint32_t;
@@ -34,71 +34,65 @@ typedef unsigned __int8 bool;
 /*!
  * \param cookie_ptr Pointer to a bincookie_cookie_t data structure.
  */
-#define bincookie_is_secure(cookie_ptr)                                       \
-    (cookie_ptr->flags & secure) /**< @hideinitializer */
+#define bincookie_is_secure(cookie_ptr) (cookie_ptr->flags & secure) /**< @hideinitializer */
 //! Check if a cookie can be used on all subdomains of its original domain.
 /*!
  * \param cookie_ptr Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_domain_access_full(cookie_ptr)                              \
-    (((char *)cookie_ptr + cookie_ptr->domain_offset)[0] ==                   \
-     '.') /**< @hideinitializer */
+#define bincookie_domain_access_full(cookie_ptr)                                                   \
+    (((char *)cookie_ptr + cookie_ptr->domain_offset)[0] == '.') /**< @hideinitializer */
 //! Initialise a bincookie_iter_state_t structure.
 /*!
  * \param s bincookie_iter_state_t structure.
  */
-#define bincookie_iter_state_init(s)                                          \
-    s.page_offset = 0;                                                        \
+#define bincookie_iter_state_init(s)                                                               \
+    s.page_offset = 0;                                                                             \
     s.page_index = 0; /**< @hideinitializer */
 //! Get the domain of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_domain(c)                                                   \
-    ((char *)c + c->domain_offset) /**< @hideinitializer */
+#define bincookie_domain(c) ((char *)c + c->domain_offset) /**< @hideinitializer */
 //! Get the path of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_path(c)                                                     \
-    ((char *)c + c->path_offset) /**< @hideinitializer                        \
+#define bincookie_path(c)                                                                          \
+    ((char *)c + c->path_offset) /**< @hideinitializer                                             \
                                   */
 //! Get the name of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_name(c)                                                     \
-    ((char *)c + c->name_offset) /**< @hideinitializer                        \
+#define bincookie_name(c)                                                                          \
+    ((char *)c + c->name_offset) /**< @hideinitializer                                             \
                                   */
 //! Get the value of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_value(c)                                                    \
-    ((char *)c + c->value_offset) /**< @hideinitializer */
+#define bincookie_value(c) ((char *)c + c->value_offset) /**< @hideinitializer */
 //! Difference between Apple's epoch (2001-01-01) and UNIX.
 #define APPLE_EPOCH_OFFSET 978307200
 //! Get the expiration time of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_expiration_time(c)                                          \
+#define bincookie_expiration_time(c)                                                               \
     (c->expiry_date_epoch + APPLE_EPOCH_OFFSET) /**< @hideinitializer */
 //! Get the creation time of a cookie.
 /*!
  * \param c Pointer to a bincookie_cookie_t structure.
  */
-#define bincookie_creation_time(c)                                            \
+#define bincookie_creation_time(c)                                                                 \
     (c->create_date_epoch + APPLE_EPOCH_OFFSET) /**< @hideinitializer */
 
 //! Security enabled for a cookie.
 /*! A cookie can be either or these values or 2 or more values OR'd together.
  */
 typedef enum {
-    secure = 1, /**< Cookie can only be used with HTTPS @hideinitializer */
-    http_only =
-        1
-        << 2, /**< Cookie cannot be accessed by JavaScript @hideinitializer */
+    secure = 1,         /**< Cookie can only be used with HTTPS @hideinitializer */
+    http_only = 1 << 2, /**< Cookie cannot be accessed by JavaScript @hideinitializer */
 } bincookie_flag;
 
 //! Keeps track of iteration state when iterating cookie file pages.
@@ -127,9 +121,9 @@ typedef struct {
 
 //! Cookie page structure. A page can consist of 1 or more cookies.
 typedef struct {
-    unsigned char unk1[4]; /*!< Has unknown purpose. Seems to be always `0x0
-                              0x0 0x1 0x0`. */
-    uint32_t num_cookies;  /*!< Number of cookies contained in this page. */
+    unsigned char unk1[4];     /*!< Has unknown purpose. Seems to be always `0x0
+                                  0x0 0x1 0x0`. */
+    uint32_t num_cookies;      /*!< Number of cookies contained in this page. */
     uint32_t cookie_offsets[]; /*!< Cookie offset values. */
 } bincookie_page_t;
 
@@ -150,34 +144,28 @@ static inline bincookie_t *const bincookie_init_file(FILE *fin) {
     rewind(fin);
     char magic[4];
     size_t read = fread(magic, sizeof(magic), 1, fin);
-    fseek(fin, last_pos, SEEK_SET);
-    bool is_cook = magic[0] == 'c' && magic[1] == 'o' && magic[2] == 'o' &&
-                   magic[3] == 'k';
+    bool is_cook = magic[0] == 'c' && magic[1] == 'o' && magic[2] == 'o' && magic[3] == 'k';
     if (read != 1 || !is_cook) {
         errno = EIO;
         return NULL;
     }
-
     // Read entire file
     fseek(fin, 0, SEEK_END);
     size_t num_bytes = (size_t)ftell(fin);
     bincookie_t *cook = malloc(num_bytes);
+    // LCOV_EXCL_START
     if (cook == NULL) {
         return NULL;
-    }
+    } // LCOV_EXCL_STOP
     memset(cook, 0, num_bytes);
     rewind(fin);
-    fread(cook, 1, num_bytes, fin);
-
+    fread(cook, num_bytes, 1, fin);
     cook->num_pages = __builtin_bswap32(cook->num_pages);
-
     // Fix page size numbers
     for (uint32_t i = 0; i < cook->num_pages; i++) {
         *(cook->page_sizes + i) = __builtin_bswap32(*(cook->page_sizes + i));
     }
-
     fseek(fin, last_pos, SEEK_SET);
-
     return cook;
 }
 //! Read a binarycookies file.
@@ -187,14 +175,11 @@ static inline bincookie_t *const bincookie_init_file(FILE *fin) {
  */
 static inline bincookie_t *const bincookie_init_path(const char *file_path) {
     FILE *binary_file = fopen(file_path, "rb");
-
     if (!binary_file) {
         return NULL;
     }
-
     bincookie_t *ret = bincookie_init_file(binary_file);
     fclose(binary_file);
-
     return ret;
 }
 //! Iterate pages of a binarycookies file.
@@ -203,16 +188,13 @@ static inline bincookie_t *const bincookie_init_path(const char *file_path) {
  \param s Pointer to bincookie_iter_state_t object.
  \return Pointer to a bincookie_page_t structure or `NULL`.
  */
-static inline bincookie_page_t *const
-bincookie_iter_pages(const bincookie_t *bc,
-                     bincookie_iter_state_t *const state) {
+static inline bincookie_page_t *const bincookie_iter_pages(const bincookie_t *bc,
+                                                           bincookie_iter_state_t *const state) {
     if (state->page_offset == 0) {
-        state->page_offset = (uint32_t)(4 + sizeof(uint32_t) +
-                                        (sizeof(uint32_t) * bc->num_pages));
+        state->page_offset = (uint32_t)(4 + sizeof(uint32_t) + (sizeof(uint32_t) * bc->num_pages));
     }
     if (state->page_index < bc->num_pages) {
-        bincookie_page_t *page =
-            (bincookie_page_t *)((char *)bc + state->page_offset);
+        bincookie_page_t *page = (bincookie_page_t *)((char *)bc + state->page_offset);
         state->page_offset += bc->page_sizes[state->page_index];
         state->page_index++;
         return page;
@@ -225,11 +207,10 @@ bincookie_iter_pages(const bincookie_t *bc,
  \param i Pointer to an integer to keep track of index.
  \return Pointer to a bincookie_cookie_t structure or `NULL`.
  */
-static inline bincookie_cookie_t *const
-bincookie_iter_cookies(const bincookie_page_t *page, unsigned int *i) {
+static inline bincookie_cookie_t *const bincookie_iter_cookies(const bincookie_page_t *page,
+                                                               unsigned int *i) {
     if (*i < page->num_cookies) {
-        bincookie_cookie_t *ptr =
-            (bincookie_cookie_t *)((char *)page + page->cookie_offsets[*i]);
+        bincookie_cookie_t *ptr = (bincookie_cookie_t *)((char *)page + page->cookie_offsets[*i]);
         (*i)++;
         return ptr;
     }
